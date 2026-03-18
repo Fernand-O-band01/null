7a00e975f178590dfc04eb199646b00bdc4ec888
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-17 19:31:58 -0600

    fix: (Checkstyle) FriendShipStatus - Done.

--- null_services_backend/src/main/java/com/example/demo/friends/FriendShipStatus.java
+++ null_services_backend/src/main/java/com/example/demo/friends/FriendShipStatus.java
@@ -1,7 +1,16 @@
 package com.example.demo.friends;
 
 public enum FriendShipStatus {
+    /**
+     * Estado pendiente de la solicitud de amistad.
+     */
     PENDING,
+    /**
+     * Estado Aceptado de la solicitud de amistad.
+     */
     ACCEPTED,
+    /**
+     * Estado Bloqueado de la solicitud de amistad.
+     */
     BLOCKED
 }
