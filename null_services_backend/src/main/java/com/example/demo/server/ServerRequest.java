05902a107ad9d10108572e12e408a4d6cf4198e7
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-19 20:19:34 -0600

    fix: (Checkstyle) ServerRequest - Done.

--- null_services_backend/src/main/java/com/example/demo/server/ServerRequest.java
+++ null_services_backend/src/main/java/com/example/demo/server/ServerRequest.java
@@ -1,13 +1,21 @@
 package com.example.demo.server;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+//import lombok.*;
 
 /**
- * DTO (Data Transfer Object) para la creación y actualización de Servidores.
+ * DTO (Data Transfer Object) para la
+ * creación y actualización de Servidores.
  * <p>
- * Define los campos necesarios que el cliente (Frontend) debe enviar al servidor
- * para dar de alta una nueva comunidad. Incluye validaciones básicas para asegurar
+ * Define los campos necesarios que el cliente (Frontend)
+ * debe enviar al servidor
+ * para dar de alta una nueva comunidad.
+ * Incluye validaciones básicas para asegurar
  * que la información mínima requerida esté presente.
  * </p>
  */
@@ -21,9 +29,10 @@ public class ServerRequest {
     /**
      * El nombre identificativo del servidor.
      * <p>
-     * Se utiliza @NotBlank para asegurar que el nombre no sea nulo, no esté vacío
-     * y no contenga únicamente espacios en blanco, garantizando que cada servidor
-     * sea visible y seleccionable en la UI.
+     * Se utiliza @NotBlank para asegurar que el nombre
+     * no sea nulo, no esté vacío
+     * y no contenga únicamente espacios en blanco, garantizando
+     * que cada servidor sea visible y seleccionable en la UI.
      * </p>
      */
     @NotBlank(message = "Server's name is required")
@@ -32,8 +41,9 @@ public class ServerRequest {
     /**
      * Ruta o URL de la imagen de perfil del servidor.
      * <p>
-     * Este campo es opcional en el envío; si no se proporciona, el sistema
-     * podría asignar una imagen por defecto o un placeholder con las iniciales
+     * Este campo es opcional en el envío;
+     * si no se proporciona, el sistema podría asignar una
+     * imagen por defecto o un placeholder con las iniciales
      * del nombre del servidor en el Frontend.
      * </p>
      */
