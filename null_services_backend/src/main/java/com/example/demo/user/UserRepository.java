de0c77d1f8000d75b929dcce4ca29fe415d40a35
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-19 21:30:34 -0600

    fix: (Checkstyle) UserRepository - Done.

--- null_services_backend/src/main/java/com/example/demo/user/UserRepository.java
+++ null_services_backend/src/main/java/com/example/demo/user/UserRepository.java
@@ -1,29 +1,34 @@
 package com.example.demo.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 /**
  * Repositorio de acceso a datos para la entidad {@link User}.
  * <p>
- * Extiende {@link JpaRepository} para heredar todas las operaciones CRUD estándar
- * (guardar, buscar, eliminar) y de paginación proporcionadas por Spring Data JPA.
+ * Extiende {@link JpaRepository} para heredar
+ * todas las operaciones CRUD estándar
+ * (guardar, buscar, eliminar) y de paginación
+ * proporcionada por Spring Data JPA.
  * </p>
  */
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     /**
-     * Busca a un usuario en la base de datos utilizando su dirección de correo electrónico.
+     * Busca a un usuario en la base de datos
+     * utilizando su dirección de correo electrónico.
      * <p>
-     * Este método es vital para el sistema de autenticación de Spring Security
-     * (dentro del UserDetailsService), ya que el email se utiliza como el identificador
+     * Este método es vital para el sistema de autenticación
+     * de Spring Security
+     * (dentro del UserDetailsService), ya que el email
+     * se utiliza como el identificador
      * principal (username) para iniciar sesión en la aplicación.
      * </p>
      *
      * @param email El correo electrónico exacto del usuario a buscar.
-     * @return Un {@link Optional} que contiene al usuario si se encuentra en la base de datos,
+     * @return Un {@link Optional} que contiene al usuario
+     * si se encuentra en la base de datos,
      * o un Optional vacío si no existe ningún usuario con ese email.
      */
     Optional<User> findByEmail(String email);
